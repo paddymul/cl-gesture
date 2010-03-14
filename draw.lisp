3548375b627850f8401890862638cb5ae206fc50
Paddy Mullen <paddy@paddy-t500.(none)>
2010-03-14 16:48:15 -0400

    made a nice with-point macro, now the eventloop only deals with points

--- draw.lisp
+++ draw.lisp
@@ -45,7 +45,7 @@
               (draw-title-height (title-text) (draw-text title-text prompt-y))
               (draw-ack (ack-text) (draw-text ack-text ack-y))
               (draw-title () (draw-title-height prompt)))
-      (dotimes (foo 5)
+      (dotimes (foo 3)
         (map-window parent)
         (format t " foo ~A gesture ~A " foo 
                 (draw-ev display  prompt-gc #'draw-title #'draw-ack))
@@ -68,42 +68,43 @@
 
 (defmacro with-point (&rest body)
   `(let ((p (make-instance 'point :x x :y y)))
-     ,body))
+     ,@body))
 
 (defun draw-ev (display prompt-gc draw-title draw-ack)
   (unwind-protect
-       (let ((last-x nil)
-             (last-y nil)
+       (let ((last-point nil)
              (gesture nil))
-         (flet ((dr-l  (window x0 y0 x1 y1)
-                  (draw-line  window prompt-gc x0 y0 x1 y1)))
+         (flet ((dr-lp (window p1 p2)
+                  (draw-line  window prompt-gc 
+                              (point-x p1) (point-y p1)
+                              (point-x p2) (point-y p2))))
            (loop
               while (not 
                      (EVENT-CASE (display :force-output-p t)
                        (:exposure (count)
-                           ;; Display prompt
-                           (when (zerop count)
-                             (funcall draw-title ))
-                           nil)
-                (:motion-notify  (window x y code)
-                                 (dr-l  window last-x last-y x y)
-                                 (setf last-x x) (setf last-y y)
-                                 (set-last-point 
-                                  gesture 
-                                  (make-instance 'point :x x :y y))
-                                 nil
-                                 )
-                (:button-release (x y)
-                                 (princ "button-release called"))
-                (:button-press (x y)
-                               (setf last-x x) (setf last-y y) 
-                               (setf gesture 
-                                     (make-gesture
-                                      (make-instance 'point :x x :y y)))
-                               (funcall draw-ack
-                                        (format nil "You have selected ~a." "foo"))
-                               nil)
-                (otherwise ()
-                           ;;Ignore and discard any other event
-                           nil))))))))
+                                  ;; Display prompt
+                                  (when (zerop count)
+                                    (funcall draw-title ))
+                                  nil)
+                       (:motion-notify  (window x y code)
+                                        (with-point 
+                                            (dr-lp  window last-point p)
+                                          (setf last-point p)
+                                          (set-last-point gesture p)
+                                          ) nil)
+                       (:button-release (x y)
+                                        (princ "button-release called")
+                                        (format t "~A" gesture)
+                                        gesture )
+                       (:button-press (x y)
+                                      (with-point 
+                                          (setf last-point p)
+                                        (setf gesture (make-gesture p))
+                                        (funcall draw-ack
+                                                 (format nil "You have selected ~a." "foo"))
+                                        )nil)
+                       (otherwise ()
+                                  ;;Ignore and discard any other event
+                                  nil))))))))
 
+(defmacro EVENT-WHILE-NIL (display force-output)
